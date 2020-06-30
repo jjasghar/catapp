775ec0acece896a324f06b89808a4604a5f9c9d7
a-roberts <aroberts@uk.ibm.com>
2020-06-30 10:10:42 +0100

    Add to helper script, use nodeport 30300

--- tekton/tkn-run.sh
+++ tekton/tkn-run.sh
@@ -2,6 +2,7 @@
 
 if [ "$#" -ne 3 ]; then
   echo "You must enter exactly three command line arguments. First the repository URL to clone, then the Docker registry URL to push to, and finally the application URL that will be curled for testing"
+  echo "Hint, you probably want ./tkn-run.sh https://github.com/a-roberts/catapp.git docker.io/<our registry> http://catapp:30300"
   exit -1
 fi
 
