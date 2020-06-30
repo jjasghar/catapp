96f32087ff9e2f4c9dae62e1b8da74f5f1a135b4
a-roberts <aroberts@uk.ibm.com>
2020-06-30 10:59:38 +0100

    It's not bad but now the clone fails

--- tekton/tkn-run.sh
+++ tekton/tkn-run.sh
@@ -10,5 +10,5 @@ repourl=$1
 dockerurl=$2
 appurl=$3
 
-echo "Make sure you've got a Docker secret for Tekton configured for $docker-url!"
-tkn pipeline start from-catalog-shelf --workspace name=shared-data,secret=docker-secret -p repo-url=${repourl} -p docker-url=${dockerurl} -p app-url=${appurl}
+echo "Make sure you've got a Docker secret for Tekton configured for $docker-url! Patched to the serviceaccount default in the namespace default"
+tkn pipeline start from-catalog-shelf --workspace name=shared-data,secret=docker-secret -p repo-url=${repourl} -p docker-url=${dockerurl} -p app-url=${appurl} -n default -s default
